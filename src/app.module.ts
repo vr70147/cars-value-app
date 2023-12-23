67b02ba686553175b7e3998f54dcbd0a1f15135d
Raanan Adut <radesigner84@gmail.com>
2023-12-23 22:16:56 +0200

    connect sqlite and create model for Users

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { ReportsController } from './reports/reports.controller';
 import { ReportsService } from './reports/reports.service';
 import { ReportsModule } from './reports/reports.module';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 @Module({
   imports: [
@@ -15,7 +16,7 @@ import { UsersModule } from './users/users.module';
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
   ],
