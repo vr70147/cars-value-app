49aca16b26d9da142f3afbe1f7f2adbb209f3dd1
Raanan Adut <radesigner84@gmail.com>
2023-12-23 22:16:56 +0200

    connect sqlite and create model for DB

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-
+import { UsersController } from './users.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 @Module({
-  providers: [UsersService]
+  imports: [TypeOrmModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UsersModule {}
