dd1ddae8a39c19f1c2d249e5529f79574645fe7a
Raanan Adut <radesigner84@gmail.com>
2023-12-23 22:33:09 +0200

    Fix import in Reports module

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
-
-@Module({})
+import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
+import { Reports } from './reports.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+@Module({
+  imports: [TypeOrmModule.forFeature([Reports])],
+  controllers: [ReportsController],
+  providers: [ReportsService],
+})
 export class ReportsModule {}
