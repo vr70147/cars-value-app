c0f60fdb088be9e95d72211985308f9dcd6ed519
Raanan Adut <radesigner84@gmail.com>
2023-12-26 23:18:14 +0200

    Check if user already exist by email

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,7 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Injectable()
 export class AuthService {
   constructor(private userService: UsersService) {}
+
+  async signup(email: string, password: string) {
+    const user = await this.userService.findAllUsers(email);
+    if (user.length) {
+      throw new BadRequestException('email in use');
+    }
+  }
+
+  signin() {}
 }
