5493661502ee330a33be52d10a9336a91bcd786c
Raanan Adut <radesigner84@gmail.com>
2023-12-30 22:29:13 +0200

    Testing signin updates session mock

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -34,7 +35,9 @@ describe('UsersController', () => {
     };
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {},
+      signin: (email: string, password: string) => {
+        return Promise.resolve({ id: 1, email, password } as User);
+      },
     };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
@@ -55,5 +58,30 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findUser = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    await expect(users.length).toEqual(1);
+    await expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    await expect(user).toBeDefined();
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: 55 };
+    const user = await controller.signin(
+      { email: 'asdf@asdf.com', password: 'mypassword' },
+      session,
+    );
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
+    console.log(session.userId);
   });
 });
