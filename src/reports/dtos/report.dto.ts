8f1d464e8c8c3b3bf71ed0495c17dbbfb4baeb93
Raanan Adut <radesigner84@gmail.com>
2024-01-01 23:21:52 +0200

    Hide user pass and email from the report's return value

--- src/reports/dtos/report.dto.ts
+++ src/reports/dtos/report.dto.ts
@@ -1,6 +1,18 @@
-import { Expose } from 'class-transformer';
+import { Expose, Transform } from 'class-transformer';
 
 export class ReportDto {
+  @Expose()
+  id: number;
+
+  @Expose()
+  price: number;
+
+  @Expose()
+  lng: number;
+
+  @Expose()
+  lat: number;
+
   @Expose()
   make: string;
 
@@ -9,4 +21,11 @@ export class ReportDto {
 
   @Expose()
   year: number;
+
+  @Expose()
+  mileage: number;
+
+  @Transform(({ obj }) => obj.user.id)
+  @Expose()
+  userId: number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,9 @@ import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from '../interceptors/serialize.interceptor';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
+
 @Controller('reports')
 @Serialize(ReportDto)
 export class ReportsController {
@@ -11,7 +14,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportsService.create(body);
+  createReport(@Body() body: CreateReportDto, @CurrentUser() user: User) {
+    return this.reportsService.create(body, user);
   }
 }
